cb138cecbe37272a30c3e8a7944af8b2375c6dbf
Mark Smith <hyperoceanic@users.noreply.github.com>
2023-11-12 16:55:36 +0000

    Fix login uri, add access_token cookie

--- main.fs
+++ main.fs
@@ -7,6 +7,9 @@ open Falco.HostBuilder
 open configuration
 open html
 open spotify
+open System.Collections.Generic
+open System.Text.Json
+open System.Web
 
 let homePageHandler : HttpHandler = fun ctx ->
 
@@ -16,12 +19,26 @@ let homePageHandler : HttpHandler = fun ctx ->
 
     Response.ofHtml page ctx
 
+let handlerWithHeader : HttpHandler =
+    Response.withCookie "greeted" "1"
+    >> Response.ofPlainText "Hello world"
+
+
 let redirectPageHandler : HttpHandler = fun ctx ->
     let config = GetSpotifyAppConfig
     let auth = buildAuth config.spotify_client_id config.spotify_client_secret
     let code : string = ctx.Request.Query["code"][0]
-    let accessToken = getAccessTokenM (auth, code)
-    Response.ofHtml (spotifyPage accessToken) ctx
+    let payload = getAccessTokenM (auth, code)
+
+    let dict = JsonSerializer.Deserialize<Dictionary<string, obj>>payload
+    let access_token = dict["access_token"] |> string
+
+    let page = spotifyPage access_token
+
+    let bar = Response.withCookie "access_token" access_token
+            >> Response.ofHtml page
+
+    bar ctx
 
 webHost [||] {
     endpoints [
--- spotify.fs
+++ spotify.fs
@@ -1,11 +1,8 @@
 module spotify
 open System
 open System.Web
-open Microsoft.AspNetCore.WebUtilities
 open System.Text
 open System.Net.Http
-open System.Text.Json
-open System.Text.Json.Serialization;
 open System.Collections.Generic
 open System.Net.Http.Headers
 
@@ -16,18 +13,17 @@ let getLoginURI spotifyClientId =
     let ub = UriBuilder "https://accounts.spotify.com/authorize"
     let query = HttpUtility.ParseQueryString(ub.Query)
     query["client_id"] <- spotifyClientId
-    query["scope"] <-  HttpUtility.UrlEncode "playlist-read-private, playlist-read-collaborative"
+    query["scope"] <- "playlist-read-private playlist-read-collaborative"
     query["response_type"] <- "code"
     query["redirect_uri"] <- callbackUri
     query["show_dialog"] <- true.ToString()
 
-    ub.ToString()
+    $"{ub}?{query}"
 
 let buildAuth clientId clientSecret =
     $"{clientId}:{clientSecret}"
     |> Encoding.UTF8.GetBytes
     |> Convert.ToBase64String
-
 
 let getAccessTokenM (auth : string, code : string) =
 
@@ -41,8 +37,7 @@ let getAccessTokenM (auth : string, code : string) =
         client.DefaultRequestHeaders.Authorization <- AuthenticationHeaderValue ("Basic", auth)
 
         let target = "https://accounts.spotify.com/api/token"
-        let httpContent = FormUrlEncodedContent content
-
+        let httpContent = new FormUrlEncodedContent (content)
         let! response = client.PostAsync(target, httpContent)
         return! response.Content.ReadAsStringAsync()
     }
